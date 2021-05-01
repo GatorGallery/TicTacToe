77e1d03590700fcd28a475b06cc01ac721dce626
ahance27 <hancea@allegheny.edu>
2021-05-01 15:36:41 -0400

    Added main file for gui implementation and user input

---
+++ src/main.py
@@ -0,0 +1,5 @@
+# Main file for TicTacToe
+# Team5Project2 - TicTacToe
+# Authors: Tommy Antle, Andre Hance, Bailey Matrascia, Alex Le Floch
+
+# This will hold the GUI, User input, and other main features.
