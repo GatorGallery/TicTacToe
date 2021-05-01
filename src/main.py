8c9f096afcf87ecb514815ce2520c04d223c7488
ahance27 <hancea@allegheny.edu>
2021-05-01 16:40:10 -0400

    created a very basic window

--- src/main.py
+++ src/main.py
@@ -4,4 +4,21 @@
 
 # This will hold the GUI, User input, and other main features.
 
+print("testing 123")
 import PySimpleGUI as sg
+
+# Creating the layout for the window
+layout = [[sg.Text("Tic-Tac-Toe")], [sg.Button("EXIT")]]
+
+# Creating the window
+window = sg.Window("TicTacToe", layout)
+
+# Create an event loop while the window is open
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the Exit button
+    if event == "EXIT" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
