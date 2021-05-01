bac5e8ac3c496594fd5bb07b4860cc2b83c4dd8a
ahance27 <hancea@allegheny.edu>
2021-05-01 19:58:02 -0400

    added an empty image, more inline comments

--- src/main.py
+++ src/main.py
@@ -4,17 +4,22 @@
 
 # This will hold the GUI, User input, and other main features.
 
+# Just for looks
 print("Starting...")
+
+# GUI will be built using PySimpleGUI
 import PySimpleGUI as sg
 
 # Setting Theme
 sg.theme('BrightColors')
 
-xPiece = '../images/xPiece.png'
-yPiece = '../images/yPiece.png'
+# piece image variables, to be assigned to buttons
+xPiece = './images/xPiece.png'
+yPiece = './images/yPiece.png'
+emptyPiece = './images/emptyPiece.png'
 
 # Creating the layout for the window
-layout = [[sg.Text("Tic-Tac-Toe")], [sg.Button(image_filename=yPiece, image_size=(100, 100), image_subsample=2,  key='1'),
+layout = [[sg.Text("Tic-Tac-Toe")], [sg.Button(image_filename=emptyPiece, image_size=(100, 100), image_subsample=2,  key='1'),
  sg.Button("2"), sg.Button("3")], [sg.Button("4"),
             sg.Button("5"), sg.Button("6")], [sg.Button("7"), sg.Button("8"), sg.Button("9")], [sg.Button("EXIT")]]
 
