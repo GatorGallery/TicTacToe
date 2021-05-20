06cca6beb7aaf19e3c9cf29308ab421649ea5007
Tommy Antle <tommyantle@MacBook-Pro.local>
2021-05-20 16:03:37 -0400

    Add test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,3 +11,9 @@ from src import main
 def test_number_one():
     """Checks."""
     assert 1 == 1
+
+
+def test_check_minmax(depth = 0, maxdepth = 10):
+    """Checks."""
+    if depth >= maxdepth:
+        assert 0
